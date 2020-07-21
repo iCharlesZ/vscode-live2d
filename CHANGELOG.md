3fcdbff0882758d258f63f0f7f454850e4a928a7
Charles zhang <zhangchixx8@gmail.com>
2020-07-21 17:57:48 +0800

    init

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,1 @@
 # Change Log
-
-All notable changes to the "vscode-live2d" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
-
-- Initial release
---
+++ src/assert/model.js
@@ -0,0 +1,23 @@
+var models = [
+  {
+    name: '看板娘',
+    models: [
+      {
+        name: 'shizuku',
+        link: 'models/ShizukuTalk/shizuku/index.json'
+      },
+      {
+        name: 'shizuku-pajama',
+        link: 'models/ShizukuTalk/shizuku-pajama/index.json'
+      },
+      {
+        name: '22',
+        link: 'models/bilibili-live/22/index.json'
+      },
+      {
+        name: '33',
+        link: 'models/bilibili-live/33/index.json'
+      }
+    ]
+  }
+];
