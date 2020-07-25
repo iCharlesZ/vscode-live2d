1c1c9013af4f63912140efbdd66c84068ac6e411
Charles Zhang <zhangchixx8@gmail.com>
2020-07-25 20:47:54 +0800

    添加了更多妹子

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,10 @@ import * as vscode from 'vscode';
 import { Main } from './Main';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('vscode-live2d.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello live2d!');
-    });
-    context.subscriptions.push(disposable);
+    // let disposable = vscode.commands.registerCommand('vscode-live2d.helloWorld', () => {
+    //     vscode.window.showInformationMessage('Hello live2d!');
+    // });
+    // context.subscriptions.push(disposable);
     context.subscriptions.push(Main.watch());
 }
 
